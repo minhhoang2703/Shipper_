839f4e14e2f948bea0be6646660bbfeba8f2b66e
Minh Hoang Tuan <102300925+minhhoang2703@users.noreply.github.com>
2023-05-14 11:53:58 +0700

    Update Order_ Details Pages

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express"); // Import Express Framework
+const app = express(); // Store express in app
+const path = require("path");
+const port = 3000;
+
+// Middleware execution for static files
+module.exports = path.dirname(require.main.filename);
+app.use(express.static(path.join(__dirname, "public"))); // Serve static files
+// Call bootstrap css file with class path
+
+// Intergrated EJS Template
+app.set("view engine", "ejs");
+app.set("views", "./views"); // setting config option for views folder
+// Intergrated EJS Template
+app.get("/", (req, res) => {
+    res.render("users/login_shipper");
+  });
+  app.get("/users/cart", (req, res) => {
+    res.render("users/shipper_dashboard");
+  });
+  app.get("/users/checkout", (req, res) => {
+    res.render("users/signup_shipper");
+  });
+  app.get("/users/contact", (req, res) => {
+    res.render("users/contact");
+});
